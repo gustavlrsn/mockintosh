19f67e30b8e0b591e6cf3e4dc7386277960e11d8
Gustav Larsson <gustav.larsson@gmail.com>
2020-05-13 22:19:01 +0200

    add post build script to copy files

---
+++ post-build.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+const copyFile = (path) => {
+  fs.copyFile(path, `./out/${path}`, (err) => {
+    if (err) throw err;
+    console.log(`${path} was copied to ./out/${path}`);
+  });
+};
+
+copyFile("README.md");
+copyFile("CONTRIBUTING.md");
+copyFile("todo.md");
