359b925bcc86d95c7e9d83724ef8f7aef5da8eed
Gustav Larsson <gustav.larsson@gmail.com>
2020-05-13 22:25:45 +0200

    comment out post build script

--- post-build.js
+++ post-build.js
@@ -1,12 +1,12 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-const copyFile = (path) => {
-  fs.copyFile(path, `./out/${path}`, (err) => {
-    if (err) throw err;
-    console.log(`${path} was copied to ./out/${path}`);
-  });
-};
+// const copyFile = (path) => {
+//   fs.copyFile(path, `./out/${path}`, (err) => {
+//     if (err) throw err;
+//     console.log(`${path} was copied to ./out/${path}`);
+//   });
+// };
 
-copyFile("README.md");
-copyFile("CONTRIBUTING.md");
-copyFile("todo.md");
+// copyFile("README.md");
+// copyFile("CONTRIBUTING.md");
+// copyFile("todo.md");
