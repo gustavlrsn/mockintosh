7e9059ea3b25508f44537aff728a2e2811dc6e09
Gustav Larsson <gustav.larsson@gmail.com>
2020-06-21 21:59:17 +0200

    add disabled menu items

--- components/menubar.js
+++ components/menubar.js
@@ -2,28 +2,22 @@ import { useState } from "react";
 import toggleFullscreen from "../utils/toggleFullscreen";
 import { windowTypes } from "../pages/index";
 
-export default ({ openWindow, zoom, setZoom }) => {
+const MenuItem = ({ children, dropdown }) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
-
+  const DropdownContent = dropdown;
   return (
-    <div className="corner-top bg-white px-2 h-5 border-b border-black flex items-stretch justify-between">
-      <div className="flex items-stretch relative">
+    <>
+      <div className="relative flex items-stretch">
+        {!dropdown && <div className="absolute inset-0 disabled"></div>}
         <button
-          className={`focus:outline-none px-2 mr-1 ${
-            dropdownOpen ? "inverted bg-white" : ""
+          className={`font-chicago focus:outline-none px-2 ${
+            (dropdownOpen ? "inverted bg-white" : "") +
+            " " +
+            (dropdown ? "cursor-pointer" : "cursor-default")
           }`}
-          onMouseDown={() => setDropdownOpen(!dropdownOpen)}
+          onMouseDown={() => dropdown && setDropdownOpen(!dropdownOpen)}
         >
-          <div className="flex items-center">
-            <img
-              src="/eaten_apple.png"
-              className=""
-              style={{
-                height: 11,
-                width: 9,
-              }}
-            />
-          </div>
+          {children}
         </button>
         {dropdownOpen && (
           <>
@@ -32,7 +26,23 @@ export default ({ openWindow, zoom, setZoom }) => {
               tabIndex="-1"
               className="z-10 fixed inset-0 h-full w-full cursor-default focus:outline-none"
             ></button>
-            <div className="absolute w-48 z-20 py-1 flex flex-col items-stretch left-0 top-0 mt-5 font-chicago bg-white border-t-0 border-b-2 border-r-2 border border-black">
+            <div className="absolute w-48 z-20 py-1 flex flex-col items-stretch left-0 top-0 mt-5 font-chicago bg-white border-t-0 border border-black shadow">
+              <DropdownContent closeDropdown={() => setDropdownOpen(false)} />
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ({ openWindow, zoom, setZoom }) => {
+  return (
+    <div className="corner-top bg-white px-2 h-5 border-b border-black flex items-stretch justify-between">
+      <div className="flex items-stretch relative">
+        <MenuItem
+          dropdown={({ closeDropdown }) => (
+            <>
               <button
                 className="px-3 py-1 focus:outline-none font-chicago text-left block hover:bg-black hover:text-white"
                 onClick={() => {
@@ -40,13 +50,12 @@ export default ({ openWindow, zoom, setZoom }) => {
                     title: "About This Mockintosh",
                     type: windowTypes.ABOUT_THIS_MOCKINTOSH,
                   });
-                  setDropdownOpen(false);
+                  closeDropdown();
                 }}
               >
                 About This Mockintosh...
               </button>
               <hr className="border-b-1 border-black border-dotted my-1" />
-
               <button
                 className="px-3 py-1 focus:outline-none font-chicago text-left block hover:bg-black hover:text-white"
                 onClick={toggleFullscreen}
@@ -66,9 +75,23 @@ export default ({ openWindow, zoom, setZoom }) => {
               >
                 View Source
               </a>
-            </div>
-          </>
-        )}
+            </>
+          )}
+        >
+          <img
+            src="/eaten_apple.png"
+            className=""
+            style={{
+              height: 11,
+              width: 9,
+            }}
+          />
+        </MenuItem>
+        <MenuItem>File</MenuItem>
+        <MenuItem>Edit</MenuItem>
+        <MenuItem>View</MenuItem>
+        <MenuItem>Label</MenuItem>
+        <MenuItem>Special</MenuItem>
       </div>
     </div>
   );
