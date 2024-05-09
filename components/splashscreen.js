67837d640f9fcc29a04bc937014ee18f419be372
Gustav Larsson <gustav.larsson@gmail.com>
2024-05-09 15:26:28 +0200

    Use Canvas image in splashscreen

--- components/splashscreen.js
+++ components/splashscreen.js
@@ -1,10 +1,12 @@
+import CanvasImage from "./canvas-image";
+
 export default function Splashscreen({ onClick }) {
   return (
     <div
       className="absolute z-40 bg top-0 bottom-0 right-0 left-0 flex items-center justify-center"
       onClick={onClick}
     >
-      <img src="/icons/happy.png" />
+      <CanvasImage src="/icons/happy.png" height={32} width={32} />
     </div>
   );
 }
